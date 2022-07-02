6e166ad58bcc9b8e64c47244ec4b3feac4566b13
lolzone13 <mohitscience123@gmail.com>
2022-07-03 01:16:46 +0530

    Updated Readme

--- Jobs/WebsiteGet.js
+++ Jobs/WebsiteGet.js
@@ -3,11 +3,10 @@ const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
 const ef = require('../Utils/emailformat');
 
-
 dotenv.config({ path: '../config/config.env' });
 
-async function sendMail(userDetails) {
-
+async function sendMail(userEmail, userDetails) {
+    console.log(userEmail);
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -16,11 +15,11 @@ async function sendMail(userDetails) {
             pass: process.env.MAIL_APP_PASSWORD
         }
     });
-    ef.emailFormat(userDetails);
-    console.log('hello', userDetails);
+    //ef.emailFormat(userDetails);
+    //console.log('hello', userDetails)
     // let info = await transporter.sendMail({
-    //     from: 'Website-Downtime-Monitor', // sender address
-    //     to: userDetails.userEmail, // list of receivers
+    //     from: '"Website-Downtime-Monitor" <website-downtime@mail.com>', // sender address
+    //     to: '', // list of receivers
     //     subject: "Hello, some of your websites might be down!", // Subject line
     //     text: "Hello, some of your websites might be down! ", // plain text body
     //     html: `<b>Website <a href=${userDetails.url}>${userDetails.name}</a> is down! </b>`, // html body
@@ -33,55 +32,45 @@ async function sendMail(userDetails) {
 
 }
 
-
-
-var currentdate = new Date(); 
-var dateNow =  currentdate.getDate() + "/" + (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear();
-var timeNow = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-
-
-
-
-async function getWebsiteData(users) {
-    
+async function getWebsiteData(data, userEmail) {
     try {
-        users.map(async (item) => {
-            const data = item.websites;
-            
-            const sites = data.map(async (ele) =>{
-                
-                const res = await axios.get(ele.url);
-                //console.log(res);
-                let newStatus = (res.status === 200) ? 'Up' : 'Down';
-                if (ele.status !== newStatus) {
-                    const statusResponse = await axios.put(process.env.URL + '/api/websites' + '/' + ele._id);
-                    
-                                            
-                    const emailObject = {
-                        website: ele,
-                        status: newStatus,
-                        date: dateNow,
-                        time: timeNow
+        for (let i = 0; i < data.length; i++) {
+            axios.get(data[i].url)
+                .then(async (res) => {
+
+                    let newStatus = 'Up';
+
+
+                    if (data[i].status !== newStatus) {
+
+                        const statusResponse = await axios.put(process.env.URL + '/api/websites' + '/' + data[i]._id);
+
+                        if (statusResponse.success === false) console.log(statusResponse.error);
+                        
                     }
-                    console.log(emailObject);
-                    return emailObject;
-                }                  
-            }); 
-            console.log('sites', sites);
-            const downSites = await Promise.all(sites);
-            console.log('downsites',downSites);
-            if (downSites.length > 0) {
-                const userDetails = {
-                    userEmail: item.userEmail,
-                    downSites: downSites
-    
-                }
-                sendMail(userDetails);
-            }
+                }).catch(async (err) => {
+                    if (err.response) {
+                        let newStatus = 'Down';
+
+                        try {
+                            if (data[i].status !== newStatus) {
+
+                                const statusResponse = await axios.put(process.env.URL + '/api/websites' + '/update/' + data[i]._id);
+
+                                if (statusResponse.success === false) console.log(statusResponse.error);
+                                sendMail(userEmail, data[i]);
+                            }
+                        } catch (error) {
+                            console.log('error');
+                        }
+
+                    }
+                });
 
 
 
-        });
+
+        }
     } catch (error) {
         console.log("Going to error", error.response.status);
     }
@@ -89,20 +78,20 @@ async function getWebsiteData(users) {
 
 
 async function getURLS(url) {
-    const URLs = [];
+    const URLs = []
     try {
         const response = await axios.get(url);
         const data = response.data.data;
         data.forEach(element => {
-            const deets = {
-                userEmail: element.email,
-                websites: element.websites
-            }
-            URLs.push(deets);
-        });
-        //console.log(URLs);
+            element.websites.forEach(val => {
+                URLs.push(val);
 
-        return URLs;
+            })
+
+        });
+
+
+        return [URLs, response.data.email];
     } catch (error) {
         console.log(error);
     }
@@ -110,6 +99,7 @@ async function getURLS(url) {
 }
 
 getURLS(process.env.URL + '/api/websites/all')
-    .then(response => getWebsiteData(response))
+    .then(response => getWebsiteData(response[0], response[1]))
+
     .catch((err) => console.log(err.data));
     // refresh
--- README.md
+++ README.md
@@ -1,3 +1,89 @@
+
 # Website Downtime Monitor
 
-Monitors the downtime of different websites.
+A simple website downtime monitoring application with alerts and uptime reports!
+
+
+## Badges
+
+[![issues](https://img.shields.io/github/issues/lolzone13/website-downtime-monitor)](https://github.com/lolzone13/website-downtime-monitor/issues) 
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+[![stars](https://img.shields.io/github/stars/lolzone13/website-downtime-monitor)](https://github.com/lolzone13/website-downtime-monitor/stargazers)
+
+
+## API Reference
+
+### Authentication Routes
+
+#### Google authentication route
+
+```http
+  GET /auth/google
+```
+
+#### Logout Route
+
+```http
+  GET /auth/logout
+
+```
+
+### Website Routes
+
+#### Gets all website details
+
+```http
+  GET /api/websites
+```
+
+#### post website details
+
+```http
+  POST /api/websites/
+```
+
+#### Delete Website
+
+```http
+  DELETE /api/websites/${id}
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to delete |
+
+#### Update Website Details
+
+```http
+  PUT /api/websites/update/${id}
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to update |
+
+## Features
+
+- Light/dark mode toggle
+- Live previews
+- Fullscreen mode
+- Cross platform
+
+
+## Tech Stack
+
+**Client:** React, TailwindCSS
+
+**Server:** Node, Express, MongoDB, Passport.js, Nodemailer
+
+
+## Contributing
+
+Contributions are always welcome!
+
+See `contributing.md` for ways to get started.
+
+Please adhere to this project's `code of conduct`.
+
